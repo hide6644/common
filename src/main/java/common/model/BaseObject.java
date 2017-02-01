f1b98862b9e3449f6acf979124e6471022dd1fcb
hide6644 <hide6644@hotmail.com>
2017-02-01 21:36:36 +0900

    リファクタリング
    hashCode、equalsメソッドの再実装を行った。

--- src/main/java/common/model/BaseObject.java
+++ src/main/java/common/model/BaseObject.java
@@ -169,44 +169,38 @@ public abstract class BaseObject {
     }
 
     /**
-     * 指定されたオブジェクトのHash Codeを生成する.
-     *
-     * @param obj
-     *            Hash Codeを生成するオブジェクト
-     * @return Hash Code
+     * {@inheritDoc}
      */
-    protected int hashCode(Object obj) {
-        return obj == null ? 0 : obj.hashCode();
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
 
     /**
-     * 指定されたオブジェクトを比較する.
-     *
-     * @param lhs
-     *            比較するオブジェクト
-     * @param rhs
-     *            比較するオブジェクト
-     * @return true 一致
+     * {@inheritDoc}
      */
-    protected boolean equals(Object lhs, Object rhs) {
-        if (lhs == rhs) {
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
             return true;
-        } else if (lhs != null) {
-            return lhs.equals(rhs);
         }
-
-        return false;
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        BaseObject other = (BaseObject) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        return true;
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public abstract int hashCode();
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public abstract boolean equals(Object obj);
 }
--- src/main/java/common/model/User.java
+++ src/main/java/common/model/User.java
@@ -460,24 +460,35 @@ public class User extends BaseObject implements Serializable, UserDetails {
      * {@inheritDoc}
      */
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof User)) {
-            return false;
-        }
-
-        User user = (User) o;
-
-        return username != null ? username.equals(user.getUsername()) : user.getUsername() == null;
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public int hashCode() {
-        return username != null ? username.hashCode() : 0;
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        User other = (User) obj;
+        if (username == null) {
+            if (other.username != null) {
+                return false;
+            }
+        } else if (!username.equals(other.username)) {
+            return false;
+        }
+        return true;
     }
 }
