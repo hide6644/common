b55e1a1ae1650284cf1b62386e28e5f1f9936e07
hide6644 <hide6644@gmail.com>
2017-10-06 10:06:14 +0900

    jQueryの更新 #7
    Ver. 3系に更新した。

--- src/main/webapp/scripts/script.js
+++ src/main/webapp/scripts/script.js
@@ -1,24 +1,23 @@
 var checkAll = function(selectAll, ids) {
-    var flag = true;
     ids.each(function() {
-        if (this.checked == false) {
-            flag = false;
+        if (this.prop('checked') == false) {
+            selectAll.prop('checked', false);
         }
     });
-    selectAll.attr('checked', flag);
+    selectAll.prop('checked', true);
 };
 
 $(function() {
     $('#toggle_checkAll').click(function() {
-        $(':checkbox[name$=Ids]:not([disabled="disabled"])').attr('checked', this.checked);
+        $(':checkbox[name$=Ids]:not([disabled="disabled"])').prop('checked', this.checked);
     });
 
     $(':checkbox[name$=Ids]:not([disabled="disabled"])').click(function() {
-        if($('#toggle_checkAll').attr('checked') == 'checked' && this.checked == false) {
-            $('#toggle_checkAll').attr('checked', false);
+        if($('#toggle_checkAll').prop('checked') && this.prop('checked') == false) {
+            $('#toggle_checkAll').prop('checked', false);
         }
 
-        if (this.checked == true) {
+        if (this.prop('checked') == true) {
             checkAll($('#toggle_checkAll'), $(':checkbox[name$=Ids]:not([disabled="disabled"])'));
         }
     });
