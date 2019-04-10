cc75a7d261533840a30fd702ce98f2d457a87e37
hide6644 <hide6644@gmail.com>
2019-04-10 17:11:43 +0900

    ライブラリのバージョンアップ
    Jarファイルを更新した。

--- src/main/java/common/service/UserSecurityAdvice.java
+++ src/main/java/common/service/UserSecurityAdvice.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.aop.AfterReturningAdvice;
