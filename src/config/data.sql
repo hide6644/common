401ca4af45e15c67dbe2c3907be88afd57aa452d
hide6644 <hide6644@gmail.com>
2020-05-21 00:33:57 +0900

    リファクタリング
    passwordEncoderをPasswordEncoderFactoriesに変更した。

--- src/config/data.sql
+++ src/config/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO app_user VALUES (-1,'administrator','$2a$10$DhTvVT74hGUfOU7qZuDAPuFpQwBZjbbjucdJjjtnPtenZFu49bkye','admin',null,'admin@foo.bar',true,false,null,null,0,null,null,null,null);
-INSERT INTO app_user VALUES (-2,'normaluser','$2a$10$.l84tRL3HL1TVL22xk5UuO01/q4loiaYyIBFZg8cmAHbHUGI8ciCC','user',null,'user@foo.bar',true,false,null,null,0,null,null,null,null);
+INSERT INTO app_user VALUES (-1,'administrator','{bcrypt}$2a$10$yaQ597EfbNuAUARw28gp4OfMLPgFI4xHD/6k.fOHepTa8bywIKuhC','admin',null,'admin@foo.bar',true,false,null,null,0,null,null,null,null);
+INSERT INTO app_user VALUES (-2,'normaluser','{bcrypt}$2a$10$/VCEQ8D6Pl1fbHGU3A2x4eVYQ/4zbmpu8lT8/76V0Pet653qyAfwG','user',null,'user@foo.bar',true,false,null,null,0,null,null,null,null);
 
 INSERT INTO role VALUES (-1,'ROLE_ADMIN','Administrator role (can edit Users)',1,null,null,null,null);
 INSERT INTO role VALUES (-2,'ROLE_USER','Default role for all Users',1,null,null,null,null);
