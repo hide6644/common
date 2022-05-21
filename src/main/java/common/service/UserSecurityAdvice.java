9d7e165e1e670f8077cdf5802d8db26fc485a4a8
hide6644 <hide6644@gmail.com>
2022-05-21 19:59:43 +0900

    SpotBugs
    BAD_PRACTICEの修正を行った。

--- src/main/java/common/service/UserSecurityAdvice.java
+++ src/main/java/common/service/UserSecurityAdvice.java
@@ -40,7 +40,7 @@ public class UserSecurityAdvice implements MethodBeforeAdvice, AfterReturningAdv
      * {@inheritDoc}
      */
     @Override
-    public void before(Method method, Object[] args, Object target) throws Throwable {
+    public void before(Method method, Object[] args, Object target) {
         SecurityContext ctx = SecurityContextHolder.getContext();
 
         if (ctx.getAuthentication() != null) {
@@ -90,7 +90,7 @@ public class UserSecurityAdvice implements MethodBeforeAdvice, AfterReturningAdv
      * {@inheritDoc}
      */
     @Override
-    public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
+    public void afterReturning(Object returnValue, Method method, Object[] args, Object target) {
         if (returnValue == null) {
             return;
         }
